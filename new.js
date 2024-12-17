de070ddd1b6317e158b4b2d2c6f329d298fa0619
EmmanSalvador <tomasemmanuel182005@gmail.com>
2024-12-17 17:12:00 +0800

    Initial website upload

---
+++ new.js
@@ -0,0 +1,34 @@
+function scrollToAboutMe() {
+    // Get the element by its ID (target-section in this case)
+    const target = document.getElementById("AboutMe");
+  
+    // Scroll to the element with smooth scrolling behavior
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+
+  
+  function scrollToExperience() {
+    // Get the element by its ID (target-section in this case)
+    const target = document.getElementById("Experience");
+  
+    // Scroll to the element with smooth scrolling behavior
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+  function scrollToGallery() {
+    // Get the element by its ID (target-section in this case)
+    const target = document.getElementById("Gallery");
+  
+    // Scroll to the element with smooth scrolling behavior
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+  function scrollToContact() {
+    // Get the element by its ID (target-section in this case)
+    const target = document.getElementById("Contacts");
+  
+    // Scroll to the element with smooth scrolling behavior
+    target.scrollIntoView({ behavior: "smooth" });
+  }
